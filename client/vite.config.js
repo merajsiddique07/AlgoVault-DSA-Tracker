4419a36bc8c20416c4725b15c392969788cc91b3
Meraj Siddique <merajsiddique9628@gmail.com>
2026-09-21 17:17:51 +0530

    backend api added

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://algovault-dsatracker.onrender.com/',
         changeOrigin: true,
       },
     },
